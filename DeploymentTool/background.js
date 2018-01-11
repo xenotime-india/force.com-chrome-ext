c735494c75c76ba9db7005b45cad0cac58348604
Xenotime <Xenotime@Appirios-MacBook-Pro.local>
2018-01-11 15:16:38 +0530

    update

--- DeploymentTool/background.js
+++ DeploymentTool/background.js
@@ -739,7 +739,7 @@ function workWithSOQL() {
                 }
                 jQuery('#container').append(jQuery(panel));
                 jQuery('#'+requestSqlData[index].table).DataTable({
-                    order: [[ requestSqlData[index].fields.indexOf('LastModifiedDate') + 1, "desc" ]]
+                    order: [[ requestSqlData[index].fields.indexOf('LastModifiedDate'), "desc" ]]
                 });
 
                 jQuery('#'+requestSqlData[index].table+' tbody').on('click', 'tr', function () {
@@ -787,7 +787,7 @@ function workWithMetaData() {
             jQuery('#container').append(jQuery(panel));
 
             jQuery('#'+requestMetadata[index].table).DataTable({
-                    order: [[ fields.indexOf('lastModifiedDate') + 1, "desc" ]]
+                    order: [[ fields.indexOf('lastModifiedDate'), "desc" ]]
                 });
 
             jQuery('#'+requestMetadata[index].table+' tbody').on('click', 'tr', function () {
