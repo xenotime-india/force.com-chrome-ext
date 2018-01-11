5db9c82e2d83f91cf67579ab43bc0477d03c8dd4
Xenotime <Xenotime@Appirios-MacBook-Pro.local>
2018-01-11 17:09:58 +0530

    update

--- DeploymentTool/background.js
+++ DeploymentTool/background.js
@@ -565,6 +565,15 @@ jQuery(function() {
                 });
             }
         });
+        requestMetadata = requestMetadata.sort(function (a, b) {
+            if (a.type < b.type) {
+                return -1;
+            }
+            if (a.type > b.type) {
+                return 1;
+            }
+            return 0;
+        });
         return workWithSOQL();
     })
     .then(function () {
