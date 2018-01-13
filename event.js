fa91e69e841466d80a3fdf119722359a6802ece4
Xenotime <Xenotime@Appirios-MacBook-Pro.local>
2018-01-13 12:45:26 +0530

    update

--- event.js
+++ event.js
@@ -2,10 +2,7 @@
 // passing the result to a callback function.
 chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
     if (req.loaded === false) {
-      chrome.tabs.executeScript(sender.tab.Id,
-      {
-        code: "console.log('loading...');var EnhanceLibIsLoaded = true;(function(){ var e = document.createElement('script'); e.setAttribute('id', 'sb-script'); e.setAttribute('mode', 'extension');   e.setAttribute('src','https://xenotime-india.github.io/force.com-chrome-ext/DeploymentTool/onload.js');  document.body.appendChild(e); })()"
-      });
+      chrome.tabs.executeScript(sender.tab.Id, {file: "inject.js"});
     }
 });
 
@@ -16,7 +13,5 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     // execute a content script that immediately sends back a message 
     // that checks for the value of a global variable which is set when
     // the library has been loaded
-    chrome.tabs.executeScript(tab.Id, {
-        code: "var check; if(typeof(EnhanceLibIsLoaded)== 'undefined' || EnhanceLibIsLoaded == false) { check = false;} else {check = true;}console.log(check);chrome.extension.sendMessage({ loaded: check});"
-    });
+    chrome.tabs.executeScript(tab.Id, {file: "browserAction.js"});
 });
